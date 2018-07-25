895fa946eef846b1f347c544f7e2410cf084c50b
rafaelcalpena <rafael.calpena@gmail.com>
2018-07-25 13:13:45 -0300

    chore: remove ui-related code

--- test/main-index.ts
+++ test/main-index.ts
@@ -15,24 +15,7 @@ import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 
-/* https://github.com/angular/angular/issues/15763#issuecomment-301618043 */
-import 'reflect-metadata';
 
-import 'zone.js/dist/zone';
-
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-
-// Avoid missing dependencies in tests
-import 'hammerjs/hammer';
-
-
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
@@ -41,12 +24,6 @@ declare const require: any;
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {
 };
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
-);
 
 // Then we find all the tests.
 const context = require.context('../ui', true, /spec$/i);
--- ui/home/states.ts
+++
@@ -1,9 +0,0 @@
-import {HomeComponent} from './home.component';
-
-export const states = [
-    {
-        name: 'home',
-        url: '/data-structures/home',
-        component: HomeComponent
-    }
-];
