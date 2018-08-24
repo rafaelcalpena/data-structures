a66625c78fba5df408c492b64dc51f2828f46988
rafaelcalpena <rafael.calpena@gmail.com>
2018-08-24 03:32:26 -0300

    feat: create collection structure

---
+++ src/collection/create-triples.ts
@@ -0,0 +1,18 @@
+/* TODO: Maybe add array support? */
+import {PointerMap} from '../pointer-map/pointer-map'
+import _ = require('lodash');
+import {MultiMap} from '../multi-map/multi-map'
+import {SSet} from '../sset/sset'
+
+export const createTriples = (triples, {hash: itemHash, item}) => {
+  if (_.isPlainObject(item)) {
+    /* for each property, add value to index */
+    /* TODO: add config to include/exclude properties
+    and nested objects */
+    const keys = Object.keys(item);
+    keys.forEach(k => {
+      triples = [...triples, [k, SSet.hashOf(item[k]), itemHash] ]
+    })
+  }
+  return triples;
+}
