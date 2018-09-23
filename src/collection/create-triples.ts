680470516406f88dc918ecb02218fb15fa2d82da
rafaelcalpena <rafael.calpena@gmail.com>
2018-09-23 20:29:54 -0300

    style: add todo

--- src/collection/create-triples.ts
+++ src/collection/create-triples.ts
@@ -9,6 +9,7 @@ export const createTriples = (triples, {hash: itemHash, item}) => {
     /* for each property, add value to index */
     /* TODO: add config to include/exclude properties
     and nested objects */
+    /* TODO check whether id is duplicated */
     const keys = Object.keys(item);
     keys.forEach((k) => {
       triples = [...triples, [k, SSet.hashOf(item[k]), itemHash] ];
