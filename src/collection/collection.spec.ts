7c7251dd0ba908d365c8c21beabecba0f25b2e47
rafaelcalpena <rafael.calpena@gmail.com>
2018-09-23 20:33:06 -0300

    test: update Collection tests

--- src/collection/collection.spec.ts
+++ src/collection/collection.spec.ts
@@ -19,24 +19,24 @@ describe('collection', () => {
       expect(collection.size()).toEqual(2);
       expect(collection.findOne({
         name: 'Harry'
-      })).toEqual(harry)
+      })).toEqual(jasmine.objectContaining(harry))
       expect(collection.findOne({
         lastName: 'Potter'
-      })).toEqual(harry)
+      })).toEqual(jasmine.objectContaining(harry))
       expect(collection.findOne({
         name: 'Harry',
         lastName: 'Potter'
-      })).toEqual(harry)
+      })).toEqual(jasmine.objectContaining(harry))
       expect(collection.findOne({
         name: 'Severus'
-      })).toEqual(snape)
+      })).toEqual(jasmine.objectContaining(snape))
       expect(collection.findOne({
         lastName: 'Snape'
-      })).toEqual(snape)
+      })).toEqual(jasmine.objectContaining(snape))
       expect(collection.findOne({
         name: 'Severus',
         lastName: 'Snape'
-      })).toEqual(snape)
+      })).toEqual(jasmine.objectContaining(snape))
       expect(collection.findOne({
         name: 'Argus',
         lastName: 'Filch'
@@ -44,6 +44,21 @@ describe('collection', () => {
       expect(collection.findOne({
         inexistentKey: true
       })).toBeUndefined()
+    })
+
+    it('should add uuid (unique id) in case id field is missing', () => {
+      let c = Collection.fromArray([]);
+      expect(() => c = c.add({
+        id: 'definedId'
+      })).not.toThrow();
+      expect(() => c = c.add({
+        anotherItem: 'anotherItem'
+      })).not.toThrow();
+      expect(c.toArray()).toContain({
+        id: 'definedId'
+      });
+      let anotherItem = c.toArray().find(i => i.anotherItem === 'anotherItem')
+      expect(anotherItem).toBeDefined();
     })
   })
 
@@ -69,11 +84,11 @@ describe('collection', () => {
       ])).not.toThrow();
       expect(collection.findOne({
         lastName: 'Potter'
-      })).toEqual(harry)
+      })).toEqual(jasmine.objectContaining(harry))
       expect(collection.findOne({
         name: 'Lilian',
         lastName: 'Potter'
-      })).toEqual(lilian)
+      })).toEqual(jasmine.objectContaining(lilian))
     })
   })
 
@@ -99,16 +114,14 @@ describe('collection', () => {
       ])).not.toThrow();
       expect(collection.find({
         lastName: 'Potter'
-      }).toArray()).toEqual([
-        harry,
-        lilian
-      ])
+      }).toArray()).toContain(
+        jasmine.objectContaining(harry),
+      )
       expect(collection.find({
-        name: 'Lilian',
         lastName: 'Potter'
-      }).toArray()).toEqual([
-        lilian
-      ])
+      }).toArray()).toContain(
+        jasmine.objectContaining(lilian),
+      )
     })
   })
 
@@ -124,10 +137,10 @@ describe('collection', () => {
       expect(c.size()).toEqual(1)
       expect(c.findOne({
         lastName: 'Weasley'
-      })).toEqual({
+      })).toEqual(jasmine.objectContaining({
         name: 'Ronald',
         lastName: 'Weasley'
-      })
+      }))
       expect(c.findOne({
         inexistentKey: 'test'
       })).toBeUndefined()
@@ -144,38 +157,41 @@ describe('collection', () => {
       c1 = Collection.fromArray([
         {
           name: 'Remus',
-          lastName: 'Lupin'
+          lastName: 'Lupin',
+          id: 'lupin'
         }
       ]);
       expect(() => r = c1.merge({
         name: 'Nymphadora',
-        lastName: 'Tonks'
+        lastName: 'Tonks',
+        id: 'tonks'
       })).not.toThrow();
       let rArr = r.toArray();
       expect(rArr.length).toEqual(2);
-      expect(rArr).toContain({
+      expect(rArr).toContain(jasmine.objectContaining({
         name:'Remus',
         lastName: 'Lupin'
-      })
-      expect(rArr).toContain({
+      }))
+      expect(rArr).toContain(jasmine.objectContaining({
         name: 'Nymphadora',
         lastName: 'Tonks'
-      })
+      }))
 
       expect(() => r = c1.merge({
         name:'Remus',
-        lastName: 'Lupin'
+        lastName: 'Lupin',
+        id: 'lupin'
       })).not.toThrow();
       rArr = r.toArray();
       expect(rArr.length).toEqual(1);
-      expect(rArr).toContain({
+      expect(rArr).toContain(jasmine.objectContaining({
         name:'Remus',
         lastName: 'Lupin'
-      })
-      expect(rArr).not.toContain({
+      }))
+      expect(rArr).not.toContain(jasmine.objectContaining({
         name: 'Nymphadora',
         lastName: 'Tonks'
-      })
+      }))
 
     })
   })
@@ -192,10 +208,10 @@ describe('collection', () => {
       expect(c.size()).toEqual(1)
       expect(c.findOne({
         lastName: 'Lovegood'
-      })).toEqual({
+      })).toEqual(jasmine.objectContaining({
         name: 'Luna',
         lastName: 'Lovegood'
-      })
+      }))
       expect(() => c = c.remove({
         name: 'Luna',
         lastName: 'Lovegood'
@@ -223,16 +239,16 @@ describe('collection', () => {
       expect(() => result = collection.toArray()).not.toThrow();
       expect(result.length).toBe(2)
       expect(result).toContain(
-        {
+        jasmine.objectContaining({
           name: 'xyz',
           lastName: 'xyz'
-        }
+        })
       )
       expect(result).toContain(
-        {
+        jasmine.objectContaining({
           name: 'abc',
           lastName: 'def'
-        }
+        })
       )
     })
   })
@@ -257,36 +273,42 @@ describe('collection', () => {
       expect(() => r = c1.union(c2)).not.toThrow();
       let rArr = r.toArray();
       expect(rArr.length).toEqual(2);
-      expect(rArr).toContain({
+      expect(rArr).toContain(jasmine.objectContaining({
         name: 'Dolores',
         lastName: 'Umbridge'
-      })
-      expect(rArr).toContain({
+      }))
+      expect(rArr.find(i => i.name === 'Dolores').id).toBeDefined();
+      expect(rArr.find(i => i.name === 'Ronald').id).toBeDefined();
+      expect(rArr).toContain(jasmine.objectContaining({
         name: 'Ronald',
         lastName: 'Weasley'
-      })
+      }))
     })
   })
 
   describe('getOne', () => {
     it('should return a value from the Collection', () => {
       let c = Collection.fromArray([
-        'firstItem'
+        {id: 'firstItem'}
       ]);
       let item;
       expect(() => item = c.getOne()).not.toThrow();
-      expect(item).toEqual('firstItem');
+      expect(item).toEqual({id: 'firstItem'});
       c = Collection.fromArray([]);
       expect(() => item = c.getOne()).not.toThrow();
       expect(item).toBeUndefined();
       c = Collection.fromArray([
-        'firstItem',
-        'secondItem',
-        'thirdItem'
+        {id: 'firstItem'},
+        {id: 'secondItem'},
+        {id: 'thirdItem'}
       ]);
       expect(() => item = c.getOne()).not.toThrow();
       /* must be any of the three */
-      expect(['firstItem', 'secondItem', 'thirdItem']).toContain(item);
+      expect([
+        {id: 'firstItem'},
+        {id:'secondItem'},
+        {id: 'thirdItem'}
+      ]).toContain(item);
     })
   })
 
@@ -295,12 +317,12 @@ describe('collection', () => {
       let c = Collection.fromArray([]);
       expect(c.isEmpty()).toBeTruthy();
       c = Collection.fromArray([
-        'firstItem'
+        {id: 'firstItem'}
       ]);
       expect(c.isEmpty()).toBeFalsy();
       c = Collection.fromArray([
-        'firstItem',
-        'secondItem'
+        {id: 'firstItem'},
+        {id: 'secondItem'}
       ]);
       expect(c.isEmpty()).toBeFalsy();
     })
@@ -309,26 +331,44 @@ describe('collection', () => {
   describe('difference', () => {
 
     it('should allow difference from another collection', () => {
-      let c = Collection.fromArray([5, 7, 8, 9, 12]),
-      c2 = Collection.fromArray([1, 2, 3, 4, 9, 6, 7]),
+      let c = Collection.fromArray([
+        {id: 5},
+        {id: 7},
+        {id: 8},
+        {id: 9},
+        {id: 12}
+      ]),
+      c2 = Collection.fromArray([
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4},
+        {id: 9},
+        {id: 6},
+        {id: 7}
+      ]),
       c3;
       expect(() => c3 = c.difference(c2)).not.toThrow();
       expect(c3.size()).toBe(3);
-      expect(c3.has(7)).toBeFalsy();
-      expect(c3.has(9)).toBeFalsy();
-      expect(c3.has(5)).toBeTruthy();
-      expect(c3.has(8)).toBeTruthy();
-      expect(c3.has(12)).toBeTruthy();
+      expect(c3.has({id: 7})).toBeFalsy();
+      expect(c3.has({id: 9})).toBeFalsy();
+      expect(c3.has({id: 5})).toBeTruthy();
+      expect(c3.has({id: 8})).toBeTruthy();
+      expect(c3.has({id: 12})).toBeTruthy();
     })
 
     it('should return self reference when difference equals self', () => {
-      let c = Collection.fromArray([5, 7, 8, 9, 12]),
-      c2 = Collection.fromArray([89, 90, 8172, 123]),
+      let c = Collection.fromArray([
+        {id: 5}, {id: 7}, {id: 8}, {id: 9}, {id: 12}
+      ]),
+      c2 = Collection.fromArray([
+        {id: 89}, {id: 90}, {id: 8172}, {id: 123}
+      ]),
       c3;
       expect(() => c3 = c.difference(c2)).not.toThrow();
       expect(c3).toBe(c);
 
-      c2 = Collection.fromArray([5]);
+      c2 = Collection.fromArray([{id: 5}]);
       expect(() => c3 = c.difference(c2)).not.toThrow();
       expect(c3).not.toBe(c);
 
@@ -338,26 +378,32 @@ describe('collection', () => {
 
   describe('iterators', () => {
     it('should contain iterator for "for ... of" loops', () => {
-      let c = Collection.fromArray([1, 2, 3, 4]);
+      let c = Collection.fromArray([{id: 1}, {id: 2}, {id: 3}, {id: 4}]);
       let str = "";
       for (let item of c) {
-        str += `${item};`;
+        str += `${item.id};`;
       }
-      expect(str).toBe('2;1;3;4;')
+      expect(str).toContain('2;')
+      expect(str).toContain('3;')
+      expect(str).toContain('4;')
+      expect(str).toContain('1;')
     })
 
     it('should contain forEach method', () => {
       const arr = [
-        'abc',
-        'def',
-        'ghi',
-        'jkl'
+        {id: 'abc'},
+        {id: 'def'},
+        {id: 'ghi'},
+        {id: 'jkl'}
       ];
       let c = Collection.fromArray(arr);
       c.forEach(item => {
-        expect(c.has(item)).toBeTruthy()
-        expect(arr.find((i) => i === item)).toEqual(item);
-        arr.splice(arr.indexOf(item), 1);
+        expect(c.has(item)).toBeTruthy();
+        let foundItem = arr.find((i) => {
+          return i.id === item.id;
+        })
+        expect(foundItem).toEqual(item);
+        arr.splice(arr.findIndex((i) => i.id === item.id), 1);
       })
       expect(arr.length).toBe(0);
     })
@@ -377,5 +423,95 @@ describe('collection', () => {
       expect(c2.has({id: 'Did'})).toBeFalsy();
     })
   })
+
+  describe('changes', () => {
+    it('should output single changes between two collections', () => {
+      let c1 = Collection.fromArray([]);
+      let c2 = Collection.fromArray([])
+
+      let diff;
+
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+
+      expect(diff.isEmpty()).toBeTruthy();
+      expect(diff.toArray()).toEqual([])
+
+      c1 = Collection.fromArray([
+        {id: 'a'},
+      ])
+      c2 = Collection.fromArray([])
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+      expect(diff.toArray()).toEqual([{
+        type: 'remove',
+        id: 'a',
+        item: {id: 'a'}
+      }])
+
+      c1 = Collection.fromArray([])
+      c2 = Collection.fromArray([{id: 'a'}]);
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+      expect(diff.toArray()).toEqual([{
+        type: 'add',
+        id: 'a',
+        item: {id: 'a'}
+      }])
+
+      c1 = Collection.fromArray([{id: 'a'}])
+      c2 = Collection.fromArray([{id: 'a', name: 'cde'}]);
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+      expect(diff.toArray()).toEqual([{
+        type: 'edit',
+        id: 'a',
+        before: {id: 'a'},
+        after: {id: 'a', name: 'cde'}
+      }]);
+
+    })
+
+    it('should output many changes between two collections', () => {
+      let c1 = Collection.fromArray([
+        {id: 'a'},
+        {id: 'c'}
+      ]);
+      let c2 = Collection.fromArray([
+        {id: 'a', name: 'b'},
+        {id: 'd', text: true}
+      ]);
+      let diff;
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+      expect(diff.toArray()).toContain({
+        type: 'edit',
+        id: 'a',
+        before: {id: 'a'},
+        after: {id: 'a', name: 'b'}
+      })
+      expect(diff.toArray()).toContain({
+        type: 'remove',
+        id: 'c',
+        item: {id: 'c'}
+      })
+      expect(diff.toArray()).toContain({
+        type: 'add',
+        id: 'd',
+        item: {id: 'd', text: true}
+      })
+      expect(diff.toArray().length).toBe(3)
+
+      c1 = Collection.fromArray([{notId: 'a'}])
+      c2 = Collection.fromArray([{notId: 'a', name: 'cde'}]);
+      expect(() => diff = c2.changesFrom(c1)).not.toThrow();
+      expect(diff.toArray().length).toBe(2);
+      expect(diff.toArray()).toContain(jasmine.objectContaining({
+        type: 'remove',
+        item: jasmine.objectContaining({notId: 'a'})
+      }))
+      expect(diff.toArray()).toContain(jasmine.objectContaining({
+        type: 'add',
+        item: jasmine.objectContaining({notId: 'a', name: 'cde'})
+      }))
+    })
+  })
+
+  /* TODO: ids must be added to Collection.fromArray([item]) items */
 
 })
