3a147478a819f7c1f45cf3b2d4b7cc406ae17d5b
rafaelcalpena <rafael.calpena@gmail.com>
2018-10-26 11:31:52 -0300

    feat: update plugins

--- src/collection/collection.ts
+++ src/collection/collection.ts
@@ -60,100 +60,224 @@ export const indexPlugin = {
     const triples = itemHashes.reduce(createTriples, []);
 
     return {
-      props: DefaultIndex.fromTriples(
-      triples,
-      ),
+      props: {
+        index: DefaultIndex.fromTriples(
+          triples,
+        ),
+        /* TODO: Use pointerMap with nested pointerMaps for inverse.
+        There is no need for multiple values in this case */
+        inverse: DefaultIndex.fromTriples(
+          triples.map((triple) => [triple[2], triple[0], triple[1]]),
+        ),
+      },
       state,
     };
   },
-  onRemove(item, itemHash, props, state) {
-    let defaultIndex = props;
+  onRemove(items, itemHashes, props, state) {
     /* TODO: foreach keys */
-    defaultIndex = _.reduce(item, (acc, value, key) => {
-      return acc.remove(key, SSet.hashOf(value), itemHash);
-    }, defaultIndex);
-    return defaultIndex;
-  },
-  onBeforeAdd(item, itemHash, props: DefaultIndex, state) {
-    if (!item.id) {
-      return {
-        continue: true,
-        message: null,
-        value: {
-          ...item,
-          id: uuid(),
-        },
-      };
-    }
+    const result = {
+      index: [],
+      inverse: [],
+    };
+    items.forEach((item, i) => {
+      const itemHash = itemHashes[i];
+      _.forEach(item, (value, key) => {
+       const valueHash = props.inverse.get(itemHash, key);
+       const indexPath = [key, valueHash, itemHash];
+       const inversePath = [itemHash, key, valueHash];
+       /* Using mutable for performance improvement */
+       result.index.push(indexPath);
+       result.inverse.push(inversePath);
+     });
+
+    });
+
     return {
-      continue: true,
-      message: null,
-      value: item,
+      index: props.index.removeMany(result.index),
+      inverse: props.inverse.removeMany(result.inverse),
     };
   },
-  onAdd(item, itemHash, props, state) {
-    let defaultIndex = props;
-    /* TODO: foreach keys */
-    defaultIndex = _.reduce(item, (acc, value, key) => {
-      return acc.add(key, SSet.hashOf(value), itemHash);
-    }, defaultIndex);
-    return defaultIndex;
+  onBeforeAdd(items, itemHashes, props: DefaultIndex, state) {
+    let result;
+    items.forEach((item) => {
+      if (!item.id) {
+        result = {
+          continue: true,
+          message: null,
+          value: {
+            ...item,
+            id: uuid(),
+          },
+        };
+      } else {
+        result = {
+          continue: true,
+          message: null,
+          value: item,
+        };
+      }
+    });
+
+    return result;
   },
-  API(state, props: DefaultIndex) {
+  onAdd(items, itemHashes, props, state) {
+    /* TODO: foreach keys */
+    let result = props;
+    items.forEach((item, i) => {
+      const itemHash = itemHashes[i];
+      result = _.reduce(item, (acc, value, key) => {
+        const valueHash = SSet.hashOf(value);
+        const newIndex = acc.index.add(key, valueHash, itemHash);
+        const newInverse = acc.inverse.add(itemHash, key, valueHash);
+        return {
+          index: newIndex,
+          inverse: newInverse,
+        };
+      }, result);
+    });
+
+    return result;
+  },
+  API(state, props, set, [collection]) {
     return {
       findOne(query) {
       /* TODO: separate props in indexed and not-indexed */
         /* Run sub-queries for each property key */
-        const results: PointerMap[] = _.reduce(
+        const results: StringSet[] = _.reduce(
           query,
           (result, value, propName) => {
             const args = [propName, SSet.hashOf(value)];
             return [
               ...result,
-              props.has(args[0], args[1]) ?
-                props.from(args[0], args[1]) :
-                PointerMap.fromObject({}),
+              props.index.has(args[0], args[1]) ?
+                props.index.from(args[0], args[1]) :
+                StringSet.fromEmpty(),
+            ];
+          },
+          [],
+        );
+        let firstKey: string;
+        if (results.length > 1) {
+          firstKey = results[0].intersection(...results.slice(1)).first();
+        } else {
+          firstKey = results[0].first();
+        }
+        return state[firstKey];
+      },
+
+      findOneHashOrigin(query) {
+          const results: StringSet[] = _.reduce(
+            query,
+            (result, value, propName) => {
+              const args = [propName, value];
+              return [
+                ...result,
+                props.index.has(args[0], args[1]) ?
+                  props.index.from(args[0], args[1]) :
+                  StringSet.fromEmpty(),
+              ];
+            },
+            [],
+          );
+          let firstKey;
+          if (results.length > 1) {
+            firstKey = results[0].intersection(...results.slice(1)).first();
+          } else {
+            firstKey = results[0].first();
+          }
+          return firstKey;
+      },
+
+      findOneHash(query) {
+      /* TODO: separate props in indexed and not-indexed */
+        /* Run sub-queries for each property key */
+        const results: StringSet[] = _.reduce(
+          query,
+          (result, value, propName) => {
+            const args = [propName, value];
+            return [
+              ...result,
+              props.index.has(args[0], args[1]) ?
+                props.index.from(args[0], args[1]) :
+                StringSet.fromEmpty(),
             ];
           },
           [],
         );
         let firstKey;
         if (results.length > 1) {
-          firstKey = results[0].keysIntersection(...results.slice(1)).firstKey();
+          firstKey = results[0].intersection(...results.slice(1)).first();
         } else {
-          firstKey = results[0].firstKey();
+          firstKey = results[0].first();
         }
         return state[firstKey];
       },
 
-      find(query) {
+      find(query): Collection {
         /* TODO: refactor/simplify with findOne */
         /* Run sub-queries for each property key */
-        const results: PointerMap[] = _.reduce(
+        const results: StringSet[] = _.reduce(
           query,
           (r, value, propName) => {
             const args = [propName, SSet.hashOf(value)];
             return [
               ...r,
-              props.has(args[0], args[1]) ?
-                props.from(args[0], args[1]) :
-                PointerMap.fromObject({}),
+              props.index.has(args[0], args[1]) ?
+                props.index.from(args[0], args[1]) :
+                StringSet.fromEmpty(),
             ];
           },
           [],
         );
         let result;
         if (results.length > 1) {
-          result = results[0].keysIntersection(...results.slice(1));
+          result = results[0].intersection(...results.slice(1));
         } else {
           result = results[0];
         }
-        result = result.toPairs().map(([k]) => state[k]);
-        return Collection.fromArray(result);
+        return collection.filterHashes(result);
+      },
+
+      findHash(query): Collection {
+        /* TODO: refactor/simplify with findOne */
+        /* Run sub-queries for each property key */
+        const results: StringSet[] = _.reduce(
+          query,
+          (r, value, propName) => {
+            /* arrays work as "OR" clause */
+            /* TODO: Add tests */
+            if (_.isArray(value)) {
+              return [
+                ...r,
+                orOperator(propName, value, props),
+              ];
+            } else {
+            const args = [propName, value];
+            return [
+              ...r,
+              props.index.has(args[0], args[1]) ?
+                props.index.from(args[0], args[1]) :
+                StringSet.fromEmpty(),
+            ];
+            }
+          },
+          [],
+        );
+        let result: StringSet;
+        if (results.length > 1) {
+          result = results[0].intersection(...results.slice(1));
+        } else {
+          result = results[0];
+        }
+        return collection.filterHashes(result);
       },
 
       getIndex() {
-        return props;
+        return props.index;
+      },
+
+      getMetadata() {
+        return props.inverse;
       },
 
     };
@@ -239,7 +363,6 @@ export class Collection {
     }
     return new Collection(
       {
-        ...this.internal,
         set: newSet,
       },
     );
